8b90f25ed945deb9171f31f2f205b5a8ae27e755
Andrew McNally <amcnally2@huskers.unl.edu>
2022-01-28 16:33:36 +0000

    added Andrew name and major

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 /**
  * Author: Xavier Herman
  * 2022-01-28
- * 
+ *
  * Hello World for Hack 1
- * 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,5 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Xavier Herman\n");
   printf("Electrical Engineering\n");
+
+    printf("Andrew McNally\n");
+  printf("Computer Engineering\n");
   return 0;
 }
