3eed212c793a42ee20a9bed817ab1bd48d618158
xman4242 <xavier.r.herman@gmail.com>
2022-01-28 10:27:44 -0600

    Added My Major

--- hello.c
+++ hello.c
@@ -5,4 +5,12 @@
  * Hello World for Hack 1
  * 
  */
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+  printf("Xavier Herman\n");
+  printf("Electrical Engineering\n");
+  return 0;
+}
