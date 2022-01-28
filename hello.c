17dad041c8736f5b9f6b935e6ab8be2d9b98a5ff
xman4242 <43799507+xman4242@users.noreply.github.com>
2022-01-28 10:42:06 -0600

    Style Fix

--- hello.c
+++ hello.c
@@ -3,12 +3,13 @@
  * 2022-01-28
  *
  * Hello World for Hack 1
- *
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 
   printf("Xavier Herman\n");
   printf("Electrical Engineering\n");
